16a4429a35975487ae9d269317c48a0eda200b65
root <root@DESKTOP-JKJ88O5>
2024-10-07 01:33:13 +0900

    server bind listen method 분리

--- socket_c/server.c
+++ socket_c/server.c
@@ -18,6 +18,74 @@ int g_clnt_count = 0;
 
 pthread_mutex_t g_mutex;
 
+void send_all_clnt(char * msg, void* arg);
+void * read_connected_clnt(void * arg);
+void serv_addr_init(struct sockaddr_in* serv_addr, int port_num);
+void serv_bind(int* serv_sock, struct sockaddr_in *serv_addr);
+void serv_listen(int* serv_sock, int backlog);
+
+
+int main(int argc, char ** argv ){
+
+	printf("port : [%s]\n",argv[1]);
+	int serv_sock; // 1. listen socket
+	int clnt_sock; // 2. communication socket
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in clnt_addr;
+
+	pthread_t serv_thread;
+	char buff[200];
+	int recv_len = 0;
+	int * clnt_sock_ptr;
+	socklen_t clnt_addr_size;
+
+
+	// mutex_init
+	pthread_mutex_init(&g_mutex,NULL);
+
+	// 1. server_socket create
+	serv_sock = socket(PF_INET,SOCK_STREAM,0);
+
+	serv_addr_init(&serv_addr,atoi(argv[1]));
+
+	// 2. server : bind
+	serv_bind(&serv_sock, &serv_addr);
+
+	// 3. server : listen
+	serv_listen(&serv_sock, 5);
+
+	// main thread
+	while(1){
+		clnt_addr_size = sizeof(clnt_addr);
+
+		// 4. server accept client
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+
+		if(clnt_sock == -1){
+			error_handling("accept() error.\n");
+		}else{
+			printf("client[%d] enter [%s]port \n",clnt_sock,argv[1]);
+		}
+
+		//critical section
+		pthread_mutex_lock(&g_mutex); 
+		g_clnt_socks[g_clnt_count++] = clnt_sock;
+		pthread_mutex_unlock(&g_mutex);
+
+		// 
+		clnt_sock_ptr = (int*)malloc(sizeof(int));
+		*clnt_sock_ptr = clnt_sock;
+		int create = pthread_create(&serv_thread, NULL, read_connected_clnt,(void*) clnt_sock_ptr);
+
+		if(create != 0){
+			free(clnt_sock_ptr);
+			error_handling("pthread_create error.\n");
+		}
+	}
+
+	return 0;
+}
+
 void send_all_clnt(char * msg, void* arg){
 
 	int my_sock = *(int*)arg;
@@ -58,12 +126,12 @@ void * read_connected_clnt(void * arg){
 	pthread_mutex_lock(&g_mutex);
 	for(int i = 0; i < g_clnt_count; i++){
 		if(clnt_sock == g_clnt_socks[i]){
-			
+
 			// move -1 
 			for(int j = i; g_clnt_count - 1; j++){
 				g_clnt_socks[j] = g_clnt_socks[j+1];
 			}
-		
+
 			// delete 
 			g_clnt_count--;
 			break;
@@ -78,76 +146,30 @@ void * read_connected_clnt(void * arg){
 	//return NULL;
 }
 
-int main(int argc, char ** argv ){
+void serv_addr_init(struct sockaddr_in* serv_addr, int port_num){
+	memset(serv_addr, 0, sizeof(*serv_addr)); // port(16bit) + ip(32bit) + sin_zero(8byte)		
 
-	printf("port : [%s]\n",argv[1]);
-	int serv_sock; // 1. listen socket
-	int clnt_sock; // 2. communication socket
-
-	pthread_t serv_thread;
-
-	struct sockaddr_in serv_addr;
-	struct sockaddr_in clnt_addr;
-	socklen_t clnt_addr_size;
-
-	pthread_mutex_init(&g_mutex,NULL);
-
-	serv_sock = socket(PF_INET,SOCK_STREAM,0);
-
-	// port(16bit) + ip(32bit) + sin_zero(8byte)		
-	memset(&serv_addr, 0, sizeof(serv_addr));	
-
-	serv_addr.sin_family = AF_INET; // ipv4
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htons(atoi(argv[1]));
-
-	// 1. server : bind
-	if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1){
-		error_handling("bind() error\n");		
-	}else{
-		printf("bind success!\n");
-	}
-
-	// 2. server : listen
-	if(listen(serv_sock, 5) == -1){
-		error_handling("listen() error\n");
-	}else{
-		printf("listen success!\n");
-	}
-
-	char buff[200];
-	int recv_len = 0;
-
-	int * clnt_sock_ptr;
-
-	// main
-	while(1){
-		clnt_addr_size = sizeof(clnt_addr);
-
-		// server accept client
-		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-
-		if(clnt_sock == -1){
-			error_handling("accept() error.\n");
-		}else{
-			printf("client[%d] enter [%s]port \n",clnt_sock,argv[1]);
-		}
-
-		//critical section
-		pthread_mutex_lock(&g_mutex); 
-		g_clnt_socks[g_clnt_count++] = clnt_sock;
-		pthread_mutex_unlock(&g_mutex);
-
-		// 
-		clnt_sock_ptr = (int*)malloc(sizeof(int));
-		*clnt_sock_ptr = clnt_sock;
-		int create = pthread_create(&serv_thread, NULL, read_connected_clnt,(void*) clnt_sock_ptr);
-
-		if(create != 0){
-			free(clnt_sock_ptr);
-			error_handling("pthread_create error.\n");
-		}
-	}
-
-	return 0;
+	serv_addr->sin_family = AF_INET; // ipv4
+	serv_addr->sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr->sin_port = htons(port_num);
 }
+
+void serv_bind(int* serv_sock, struct sockaddr_in *serv_addr){
+
+	if(bind(*serv_sock, (struct sockaddr*)serv_addr, sizeof(*serv_addr))==-1){
+		error_handling("bind() error\n");		
+		return;
+	}
+
+	printf("bind success!\n");
+}
+
+void serv_listen(int* serv_sock, int backlog){
+	if(listen(*serv_sock, backlog) == -1){
+		error_handling("listen() error\n");
+		return;
+	}
+
+	printf("listen success!\n");
+}
+
